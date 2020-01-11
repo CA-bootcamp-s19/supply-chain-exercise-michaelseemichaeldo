ec35e3624dda980108b0e9b5a8dbc3874c09e7a3
michaelseemichaeldo <michael.candreia@gmail.com>
2020-01-11 16:06:55 +0100

    no changes

--- truffle-config.js
+++ truffle-config.js
@@ -3,15 +3,15 @@ module.exports = {
 //     solc: '0.4.25'
 //   },
   networks: {
-    // development: {
-    //   host: "localhost",
-    //   port: 8545,
-    //   network_id: "*" // Match any network id
-    // }
-    ganache: {
-      host: '127.0.0.1',
-      port: 7545,
-      network_id: '*' // Match any network id
-    },
+    development: {
+       host: "localhost",
+       port: 8545,
+       network_id: "*" // Match any network id
+    }
+    // ganache: {
+    //   host: '127.0.0.1',
+    //   port: 7545,
+    //   network_id: '*' // Match any network id
+    // },
   }
 };
